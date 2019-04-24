692d272069c7cb5dfb874bc4708f676d4c77b0d0
TOMB <33571599+Conotox@users.noreply.github.com>
2019-04-24 22:23:49 +0100

    Add files via upload

---
+++ website timer.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+	"runtime"
+	"time"
+)
+
+func scheduleThis(x string, y time.Duration) {
+	time.Sleep(y * time.Second)
+	openBrowser(x)
+}
+
+func x() {
+	fmt.Println("this is function x")
+}
+
+func openBrowser(url string) {
+	var err error
+
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	fmt.Println("This is a simple website timer. Please type in a time in seconds.")
+	var i time.Duration
+	var web string
+	fmt.Scan(&i)
+	fmt.Println("Now type in a website (hint, start with https://)")
+	fmt.Scan(&web)
+	scheduleThis(web, i)
+}
